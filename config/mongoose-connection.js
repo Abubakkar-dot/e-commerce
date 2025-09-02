613f9f18ebc7e0d4c17ac7cf88d9e548b16f8e0c
Abubakkar-dot <abutera55778@gmail.com>
2025-09-02 22:00:39 +0500

    First deploy setup

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -46,16 +46,19 @@
 
 
 
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
-
-app.get("/", (req, res) => {
-  res.send("✅ Session store connected!");
-});
-
-app.listen(8080, () => console.log("Listening on 8080"));
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const express = require("express");
+const app = express();
+app.use(session({
+  secret: "yourSecretKey", // change to a strong secret
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI, // use your MongoDB connection
+    collectionName: "sessions"
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+  }
+}));
