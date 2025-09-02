b290ed79df92e78a9cc7c838df2a49c51ba8ea96
Abubakkar-dot <abutera55778@gmail.com>
2025-09-02 21:08:07 +0500

    ready for deploy

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -49,6 +49,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
